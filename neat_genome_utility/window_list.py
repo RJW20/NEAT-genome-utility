262bfa04dce0a6fc1f5a03c52b48c4f769e61ccd
RJW20 <robertjwatts84@outlook.com>
2024-04-30 12:53:41 +0100

    Add way to identify GenomeWindows

--- neat_genome_utility/window_list.py
+++ neat_genome_utility/window_list.py
@@ -3,20 +3,31 @@ from neat_genome_utility.genome.genome_window import GenomeWindow
 
 class WindowList(list):
     """Subclass of built-in list that automatically shows new windows and removes
-    windows when they are closed."""
+    windows when they are closed.
+    
+    Also automatically sets the names of the windows for easy tracking when doing 
+    crossovers.
+    """
 
+    genome_id = 0
 
     def append(self, gw: GenomeWindow) -> None:
         super().append(gw)
+        self.genome_id += 1
+        gw.setWindowTitle(f"Genome {self.genome_id}")
         gw.show()
 
     def extend(self, gws: list[GenomeWindow]) -> None:
         super().extend(gws)
         for gw in gws:
+            self.genome_id += 1
+            gw.setWindowTitle(f"Genome {self.genome_id}")
             gw.show()
 
     def insert(self, i: int, gw: GenomeWindow) -> None:
         super().insert(i, gw)
+        self.genome_id += 1
+        gw.setWindowTitle(f"Genome {self.genome_id}")
         gw.show()
 
     def remove(self, gw: GenomeWindow) -> None:
